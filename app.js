5870efb79478b375f15c695fed76b2efbd6c268d
Kody9 <58841270+Kody9@users.noreply.github.com>
2023-12-12 17:58:51 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.gallery-item img').forEach(item => {
+    item.addEventListener('click', () => {
+        // Implement lightbox functionality or other interactions
+        
+    });
+});
